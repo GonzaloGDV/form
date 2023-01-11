32f386ec362ecae1a86658acf8e4e2ff27f0b5ed
Gonzalo Gomez <gonzalogdv@gmail.com>
2023-01-11 16:33:43 -0300

    Final push

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import data from '../assets/db.json';
-import { useNavigate } from 'react-router-dom';
 import styles from '../styles/Form.module.css';
 import { database } from '../components/firebase';
 import { collection, addDoc } from 'firebase/firestore';
-import { confirmAlert } from 'react-confirm-alert';
-import 'react-confirm-alert/src/react-confirm-alert.css';
 import Header from '../components/Header';
+import { useNavigate } from 'react-router-dom';
 
 const Form = () => {
   const collectionRef = collection(database, 'answers');
   const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
   const [input, setInput] = useState({
     full_name: '',
     email: '',
@@ -18,12 +17,20 @@ const Form = () => {
     country_of_origin: '',
     terms_and_conditions: '',
   });
+  const [disabledButton, setDisabledButton] = useState(true);
+  const [terms, setTerms] = useState(true);
 
   function handlerInputChange(e) {
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
+    setErrors(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
   }
 
   function handlerSelect(e) {
@@ -31,6 +38,16 @@ const Form = () => {
       ...input,
       country_of_origin: e.target.value,
     });
+    setErrors(
+      validate({
+        ...input,
+        country_of_origin: e.target.value,
+      })
+    );
+  }
+
+  function handlerOnClick() {
+    return setTerms(!terms);
   }
 
   function handlerSubmit() {
@@ -39,26 +56,11 @@ const Form = () => {
     };
     addDoc(collectionRef, { answer })
       .then(() => {
-        // alert('Información agregada');
-        confirmAlert({
-          title: 'Información agregada a la DB',
-          message: '¿Quieres ver todas las respuestas?',
-          buttons: [
-            {
-              label: 'Sí',
-              onClick: () => navigate('/answers'),
-            },
-            {
-              label: 'No',
-              onClick: null,
-            },
-          ],
-        });
+        alert('Información agregada');
       })
       .catch((err) => {
         console.log(err.message);
       });
-
     setInput({
       full_name: '',
       email: '',
@@ -66,7 +68,53 @@ const Form = () => {
       country_of_origin: '',
       terms_and_conditions: '',
     });
+    setTimeout(() => {
+      navigate('/answers');
+    }, 500);
   }
+
+  function validate(input) {
+    let errors = {};
+    if (!input.full_name) {
+      errors.full_name = 'El nombre completo es requerido';
+    } else if (!/^[a-zA-Z]+(([a-zA-Z ])?[a-zA-Z]*)*$/.test(input.full_name)) {
+      errors.full_name = 'Nombre no válido. Solo debe contener letras';
+    }
+
+    if (!input.email) {
+      errors.email = 'El correo electrónico es requerido';
+    } else if (
+      !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        input.email
+      )
+    ) {
+      errors.email = 'El formato de email debe ser válido.';
+    }
+    if (!input.country_of_origin) {
+      errors.country_of_origin = 'El país de origen es requerido.';
+    }
+    if (!input.birth_date) {
+      errors.birth_date = 'La fecha de nacimiento es requerida.';
+    }
+    if (terms === true) {
+      errors.terms_and_conditions = 'Los términos deben ser aceptados.';
+    }
+    return errors;
+  }
+
+  useEffect(() => {
+    if (
+      input.country_of_origin.length < 1 ||
+      errors.hasOwnProperty('full_name') ||
+      errors.hasOwnProperty('email') ||
+      errors.hasOwnProperty('birth_date') ||
+      terms === true
+    ) {
+      setDisabledButton(true);
+    } else {
+      setDisabledButton(false);
+    }
+  }, [errors, terms, input, setDisabledButton]);
 
   return (
     <div className={styles.container}>
@@ -81,7 +129,6 @@ const Form = () => {
                   onChange={handlerSelect}
                   className='form-select mt-4'
                   aria-label={name}
-                  required
                 >
                   <option default>{label}</option>
                   {options &&
@@ -93,10 +140,14 @@ const Form = () => {
                       );
                     })}
                 </select>
+                {errors.country_of_origin && (
+                  <p className={styles.danger}>{errors.country_of_origin}</p>
+                )}
               </div>
             ) : JSON.stringify({ type }) ===
               JSON.stringify({ type: 'submit' }) ? (
               <button
+                disabled={disabledButton}
                 type={type}
                 key={index}
                 onClick={handlerSubmit}
@@ -111,13 +162,17 @@ const Form = () => {
                   {label}
                 </label>
                 <input
-                  type={type}
                   className='form-check-input bg-danger'
+                  type={type}
                   name='terms_and_conditions'
-                  value={input.terms_and_conditions}
                   id={name}
+                  value={input.terms_and_conditions}
+                  onClick={() => handlerOnClick()}
                   required
                 />
+                {terms && (
+                  <p className={styles.danger}>{errors.terms_and_conditions}</p>
+                )}
               </div>
             ) : JSON.stringify({ type }) ===
               JSON.stringify({ type: 'text' }) ? (
@@ -134,6 +189,9 @@ const Form = () => {
                   onChange={handlerInputChange}
                   required
                 />
+                {errors.full_name && (
+                  <p className={styles.danger}>{errors.full_name}</p>
+                )}
               </div>
             ) : JSON.stringify({ type }) ===
               JSON.stringify({ type: 'email' }) ? (
@@ -150,6 +208,9 @@ const Form = () => {
                   onChange={handlerInputChange}
                   required
                 />
+                {errors.email && (
+                  <p className={styles.danger}>{errors.email}</p>
+                )}
               </div>
             ) : (
               <div className='mb-3' key={index}>
@@ -165,6 +226,9 @@ const Form = () => {
                   onChange={handlerInputChange}
                   required
                 />
+                {errors.birth_date && (
+                  <p className={styles.danger}>{errors.birth_date}</p>
+                )}
               </div>
             );
           })}
